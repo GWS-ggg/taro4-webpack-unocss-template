72fe8cecde4b3da6bfab740a3637daf77ce24cd4
格瓦斯 <v_kvass@aladinfun.com>
2024-12-10 14:22:56 +0800

    feat: taro4+webpack+unocss

---
+++ src/pages/index/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <view class="index">
+    <view className="color-red">test</view>
+    <text>{{ msg }}</text>
+  </view>
+</template>
+
+<script>
+import { ref } from 'vue'
+import './index.scss'
+
+export default {
+  setup () {
+    const msg = ref('Hello world')
+    return {
+      msg
+    }
+  }
+}
+</script>
